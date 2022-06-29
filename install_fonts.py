06abf9cfa4c19c345ca70064ad0acc5550e0f8ab
mahianmahin <mahianmahin@yahoo.com>
2022-06-30 01:41:43 +0600

    final commit

--- install_fonts.py
+++ install_fonts.py
@@ -1,8 +1,9 @@
 import os
 
+
 def install_fonts():
 	# scanning for the font file in the root dir
-	for root, dirs, files in os.walk('./', topdown=False):
+	for root, dirs, files in os.walk('./fonts/', topdown=False):
 		for name in files:
 			if '.ttf' in name:
 				os.system(f"install -m644 {name} /usr/share/fonts/")
